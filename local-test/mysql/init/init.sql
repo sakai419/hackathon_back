79a99280a7686dbbb002072651fad6c192053729
sakai419 <sakaitaiki.soccer@icloud.com>
2024-09-24 16:56:09 +0900

    Add tables

--- local-test/mysql/init/init.sql
+++ local-test/mysql/init/init.sql
@@ -5,10 +5,49 @@ CREATE TABLE accounts (
     user_id VARCHAR(28) UNIQUE NOT NULL,
     user_name VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    KEY idx_accounts_user_name (user_name)
 );
 
-CREATE INDEX idx_accounts_user_id ON accounts (user_id);
-CREATE INDEX idx_accounts_user_name ON accounts (user_name);
+DROP TABLE IF EXISTS profiles;
 
-INSERT INTO accounts (id, user_id, user_name) VALUES ('1', '1', 'user1');
+CREATE TABLE profiles (
+    account_id CHAR(28) PRIMARY KEY,
+    bio TEXT,
+    profile_image_url VARCHAR(2083),
+    banner_image_url VARCHAR(2083),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_profiles_account FOREIGN KEY (account_id)
+        REFERENCES accounts(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS settings;
+
+CREATE TABLE settings (
+    account_id CHAR(28) PRIMARY KEY,
+    is_private BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_settings_account FOREIGN KEY (account_id)
+        REFERENCES accounts(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS notifications;
+
+CREATE TABLE notifications (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    recipient_account_id CHAR(28) NOT NULL,
+    sender_account_id CHAR(28),
+    type VARCHAR(50) NOT NULL,
+    content TEXT,
+    is_read BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_notifications_resipient FOREIGN KEY (recipient_account_id)
+        REFERENCES accounts(id) ON DELETE CASCADE,
+    CONSTRAINT fk_notifications_sender FOREIGN KEY (sender_account_id)
+        REFERENCES accounts(id) ON DELETE SET NULL,
+    INDEX idx_notifications_recipient (recipient_account_id),
+    INDEX idx_notifications_created_at (created_at)
+);
