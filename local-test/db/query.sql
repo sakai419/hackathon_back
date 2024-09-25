c1f037d688e0db4b0ba18bd148b36b612fb30719
sakai419 <sakaitaiki.soccer@icloud.com>
2024-09-25 15:38:25 +0900

    Add querys of accounts and profiles

--- local-test/db/query.sql
+++ local-test/db/query.sql
@@ -1,4 +1,44 @@
 -- name: GetAccountById :one
-SELECT * FROM account WHERE id = ?;
+SELECT * FROM accounts WHERE id = ?;
 
--- name: GetAccountsByUserId :many
+-- name: GetAccountByUserId :one
+SELECT * FROM accounts WHERE user_id = ?;
+
+-- name: SearchAccountsByUserId :many
+SELECT * FROM accounts WHERE user_id LIKE "%?%";
+
+-- name: GetAccountByUserName :one
+SELECT * FROM accounts WHERE user_name = ?;
+
+-- name: SearchAccountsByUserName :many
+SELECT * FROM accounts WHERE user_name LIKE "%?%";
+
+-- name: CreateAccount :one
+INSERT INTO accounts (id, user_id, user_name) VALUES (?, ?, ?);
+
+-- name: UpdateAccountUserId :one
+UPDATE accounts SET user_id = ? WHERE id = ?;
+
+-- name: UpdateAccountUserName :one
+UPDATE accounts SET user_name = ? WHERE id = ?;
+
+-- name: DeleteAccount :one
+DELETE FROM accounts WHERE id = ?;
+
+-- name: GetProfileByAccountId :one
+SELECT * FROM profiles WHERE account_id = ?;
+
+-- name: CreateProfile :one
+INSERT INTO profiles (account_id, bio, profile_image_url, banner_image_url) VALUES (?, ?, ?, ?);
+
+-- name: UpdateProfileBio :one
+UPDATE profiles SET bio = ? WHERE account_id = ?;
+
+-- name: UpdateProfileProfileImageUrl :one
+UPDATE profiles SET profile_image_url = ? WHERE account_id = ?;
+
+-- name: UpdateProfileBannerImageUrl :one
+UPDATE profiles SET banner_image_url = ? WHERE account_id = ?;
+
+-- name: DeleteProfile :one
+DELETE FROM profiles WHERE account_id = ?;
