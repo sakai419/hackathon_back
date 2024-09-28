d693b7259a3a4787f22838e4a1ae3005327c8331
sakai419 <sakaitaiki.soccer@icloud.com>
2024-09-28 18:54:52 +0900

    Add likes queries

--- local-test/db/query.sql
+++ local-test/db/query.sql
@@ -611,3 +611,27 @@ LEFT JOIN labels l ON t.id = l.tweet_id
 WHERE l.tweet_id IS NULL
 ORDER BY t.created_at DESC
 LIMIT ? OFFSET ?;
+
+-- name: CreateLike :exec
+INSERT INTO likes (liking_account_id, original_tweet_id)
+VALUES (?, ?);
+
+-- name: DeleteLike :exec
+DELETE FROM likes
+WHERE liking_account_id = ? AND original_tweet_id = ?;
+
+-- name: GetLikesByTweetId :many
+SELECT * FROM likes
+WHERE original_tweet_id = ?
+ORDER BY created_at DESC
+LIMIT ? OFFSET ?;
+
+-- name: GetLikesByAccountId :many
+SELECT * FROM likes
+WHERE liking_account_id = ?
+ORDER BY created_at DESC
+LIMIT ? OFFSET ?;
+
+-- name: GetLikeCount :one
+SELECT COUNT(*) FROM likes
+WHERE original_tweet_id = ?;
