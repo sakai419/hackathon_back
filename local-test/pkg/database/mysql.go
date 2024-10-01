cb964c4cd2b9f00ea8ead0959bb166ef6132eb93
sakai419 <sakaitaiki.soccer@icloud.com>
2024-10-01 10:13:51 +0900

    Imprement mysql.go

--- local-test/pkg/database/mysql.go
+++ local-test/pkg/database/mysql.go
@@ -1,1 +1,27 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type DBConfig struct {
+	User string
+	Pwd string
+	Host string
+	Database string
+}
+
+func ConnectToMysql(c DBConfig) (db *sql.DB, err error) {
+    connStr := fmt.Sprintf("%s:%s@%s/%s", c.User, c.Pwd, c.Host, c.Database)
+
+    db, err = sql.Open("mysql", connStr)
+	if err != nil {
+		err = fmt.Errorf("fail: sql.Open, %v\n", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("fail: db.Ping, %v\n", err)
+	}
+	return db, err
+}
