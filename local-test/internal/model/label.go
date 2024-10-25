b11c9cea948664b3d63b146b797f406c95cdc3c1
sakai419 <sakaitaiki.soccer@icloud.com>
2024-10-25 23:04:04 +0900

    Change database

---
+++ local-test/internal/model/label.go
@@ -0,0 +1,14 @@
+package model
+
+type Label string
+
+const (
+
+)
+
+type LabelTweetParams struct {
+	TweetID string
+	Label1  string
+	Label2  string
+	Label3  string
+}
---
+++ local-test/internal/repository/label.go
@@ -0,0 +1,10 @@
+package repository
+
+import (
+	"context"
+	"local-test/internal/model"
+)
+
+func (r *Repository) LabelTweet(ctx context.Context, params *model.LabelTweetParams) error {
+	return nil
+}
