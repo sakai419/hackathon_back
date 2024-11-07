dad13bc81118062431ab0eb1b26637be9d7dd812
sakai419 <sakaitaiki.soccer@icloud.com>
2024-11-07 23:32:28 +0900

    Remove log

--- local-test/internal/handler/sidebar/handler.go
+++ local-test/internal/handler/sidebar/handler.go
@@ -4,7 +4,6 @@ import (
 	"local-test/internal/service"
 	"local-test/pkg/apperrors"
 	"local-test/pkg/utils"
-	"log"
 	"net/http"
 )
 
@@ -33,8 +32,6 @@ func (h *SidebarHandler) GetSidebarInfo(w http.ResponseWriter, r *http.Request) 
 		utils.RespondError(w, apperrors.NewHandlerError("get sidebar info", err))
 		return
 	}
-
-	log.Println("Sidebar info:", sidebarInfo)
 
 	utils.Respond(w, SidebarInfo{
 		UserInfo: UserInfoWithoutBio{
