6b3816351f593b68fb5d1e8818f7dd54aa21ce0b
sakai419 <sakaitaiki.soccer@icloud.com>
2024-12-03 18:38:58 +0900

    Modify frontend path

--- hackathon/internal/middleware/cors.go
+++ hackathon/internal/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 func EnableCorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set the headers for the preflight request
-		w.Header().Set("Access-Control-Allow-Origin", "hackathon-front-orpin.vercel.app")
+		w.Header().Set("Access-Control-Allow-Origin", "https://hackathon-front-orpin.vercel.app")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
