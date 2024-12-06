c2790691a778bb5b682af56595d3b9e222611cd4
sakai419 <sakaitaiki.soccer@icloud.com>
2024-12-07 01:14:57 +0900

    Modify port

--- gcc-server/main.go
+++ gcc-server/main.go
@@ -24,7 +24,7 @@ type CompileResponse struct {
 func main() {
 	http.HandleFunc("/compile", handleCompile)
 
-	port := "9090"
+	port := "9000"
 	fmt.Printf("GCC server is running on port %s\n", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Printf("Failed to start server: %v\n", err)
